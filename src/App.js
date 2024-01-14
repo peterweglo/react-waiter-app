fe7aee4e7ed83452b8891ecbb7abcaf1c80975ad
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-01-14 14:31:46 +0100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Table from './components/pages/Table/Table';
+import NotFound from './components/pages/NotFound/NotFound';
+
 const App = () => {
   return (
     <Container>
-      <div>Hello world”</div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/table/:id' element={<Table />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </Container>
   );
 };
