c6286cc9362c56b371f769b3ea01c55cdfaee551
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-01-15 21:06:21 +0100

    Add bill form to EditTable component

--- src/components/features/EditTable/EditTable.js
+++ src/components/features/EditTable/EditTable.js
@@ -13,6 +13,7 @@ const EditTable = () => {
   const [maxPeopleAmount, setMaxPeopleAmount] = useState(
     tableData?.maxPeopleAmount
   );
+  const [bill, setBill] = useState(tableData?.bill);
 
   console.log(tableData);
 
@@ -24,10 +25,18 @@ const EditTable = () => {
 
       <Form>
         <Form.Group as={Row} className='mb-4 mt-4'>
-          <Form.Label column sm='2' lg='1' className='fw-bold'>
+          <Form.Label
+            column
+            xs={3}
+            sm={2}
+            md={1}
+            lg={1}
+            xl={1}
+            className='fw-bold'
+          >
             Status:
           </Form.Label>
-          <Col sm='4' lg='3'>
+          <Col xs={9} sm={7} md={7} lg={5} xl={3}>
             <Form.Select
               name='status'
               aria-label='Select status'
@@ -42,12 +51,20 @@ const EditTable = () => {
           </Col>
         </Form.Group>
         <Form.Group as={Row} className='mb-4 mt-4'>
-          <Form.Label column sm='2' lg='1' className='fw-bold'>
+          <Form.Label
+            column
+            xs={3}
+            sm={2}
+            md={1}
+            lg={1}
+            xl={1}
+            className='fw-bold'
+          >
             People:
           </Form.Label>
-          <Col sm='4' lg='3'>
+          <Col>
             <Row>
-              <Col>
+              <Col xs={4} sm={3} md={3} lg={2} xl={1}>
                 <Form.Control
                   name='peopleAmount'
                   type='number'
@@ -58,10 +75,13 @@ const EditTable = () => {
                   onChange={(e) => setPeopleAmount(e.target.value)}
                 />
               </Col>
-              <Col className='d-flex align-items-center justify-content-center'>
+              <Col
+                xs={1}
+                className='d-flex align-items-center justify-content-center'
+              >
                 <span>/</span>
               </Col>
-              <Col>
+              <Col xs={4} sm={3} md={3} lg={2} xl={1}>
                 <Form.Control
                   name='maxPeopleAmount'
                   type='number'
@@ -75,6 +95,38 @@ const EditTable = () => {
             </Row>
           </Col>
         </Form.Group>
+        {tableStatus === 'Busy' && (
+          <Form.Group as={Row} className='my-3'>
+            <Form.Label
+              column
+              xs={3}
+              sm={2}
+              md={1}
+              lg={1}
+              xl={1}
+              className='fw-bold'
+            >
+              Bill:
+            </Form.Label>
+            <Col>
+              <Row>
+                <Col xs={4} sm={3} md={3} lg={2} xl={2}>
+                  <Form.Control
+                    name='bill'
+                    className='text-center'
+                    type='number'
+                    min='0'
+                    value={bill}
+                    onChange={(e) => setBill(e.target.value)}
+                  />
+                </Col>
+                <Col xs={1}>
+                  <p className='my-1'>$</p>
+                </Col>
+              </Row>
+            </Col>
+          </Form.Group>
+        )}
 
         <Button type='submit'>Update</Button>
       </Form>
